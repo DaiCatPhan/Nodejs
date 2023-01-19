66192858dd9d0fa76037f0c211f6c15980986c95
Dai Cat <phandaicat12032002@gmail.com>
2023-01-19 17:10:01 +0700

    prettier husky kint-staged

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// middleware xu li form du lieu
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resoures/views'));
+
+// Router init
+const route = require('./routes');
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
